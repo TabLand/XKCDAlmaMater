4b875bc15d09fa9da55090210cbaef3840194493
Ijtaba Hussain <ijtaba.hussain.1@city.ac.uk>
2013-04-03 04:29:31 +0100

    target string needs formating. I'm starting to think Randall is playing an April fools joke

--- skein_test.c
+++ skein_test.c
@@ -38,18 +38,23 @@ void hashit(const char *msg, int nbytes, size_t hashbitlen, int verbose)
 }
 
 int distanceCalculator(char *hash){
-  int len = sizeof(hash)  / sizeof(char);
+  unsigned char *target = "5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fdf9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6";
+  
   int i =0;
   int total = 0;
-  for(i=0; i<=len; i++){
-      total += hash[i] ^ target[i];
+  
+  for(i=0; i<1024; i++){
+       if((hash[i] ^ target[i]) & 0x1) 
+      { total = total + 1;
+      };
+      
   }
-  total = 1024 - total;
-  printf("total bits wrong by " << total << "bits");
+  printf("\n%dth step - total: %d \n",i,total);
+  printf ("total bits wrong by %d bits", total);
   return total;
 }
 
-char *target = "5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fdf9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6";
+
 // *************************************************************************
 
 int main(int argc, char **argv)
